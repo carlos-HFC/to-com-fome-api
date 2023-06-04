2373763b4506f8c28561929cfb40a8add301adc1
Carlos <carlos.faustino@bcrcx.com>
2023-06-04 19:41:35 -0300

    pegar variaveis env

--- _config/connection.php
+++ _config/connection.php
@@ -3,20 +3,24 @@
 class Connection
 {
   protected $connection;
-  private $servername = "localhost";
-  private $database = "toComFome";
-  private $username = "root";
-  private $password = "";
+  private $servername = null;
+  private $database = null;
+  private $username = null;
+  private $password = null;
 
 
   private function conectar()
   {
+    $this->servername = is_null($_ENV['MYSQL_HOST']) ? "localhost" : $_ENV['MYSQL_HOST'];
+    $this->username = is_null($_ENV['MYSQL_USER']) ? "root" : $_ENV['MYSQL_USER'];
+    $this->password = is_null($_ENV['MYSQL_ROOT_PASSWORD']) ? "" : $_ENV['MYSQL_ROOT_PASSWORD'];
+    $this->database = is_null($_ENV['MYSQL_DATABASE']) ? "toComFome" : $_ENV['MYSQL_DATABASE'];
     $this->connection = new mysqli($this->servername, $this->username, $this->password, $this->database);
   }
 
   public function __construct()
   {
-    $this->conectar(); // se conecta ao instanciar a classe
+    $this->conectar();
   }
 
   public function query($sql)
@@ -24,27 +28,17 @@ class Connection
     return $this->connection->query($sql);
   }
 
-  // function getConexao()
-  // {
+  function getConexao()
+  {
 
-  //   if (empty($this->conexao)) {
-  //     $this->conectar(); // se conecta case ainda a conexão ainda não exista
-  //   }
+    if (empty($this->connection)) {
+      $this->conectar();
+    }
 
-  //   if (empty($this->conexao) || ($this->conexao->connect_errno)) {
-  //     die("Falha na conexão: " . ' Erro Num: ' . $this->conexao->connect_errno . ' - Mensagem: ' . mysqli_connect_error());
-  //   }
+    if (empty($this->connection) || ($this->connection->connect_errno)) {
+      return "Falha na conexão: " . ' Erro Num: ' . $this->connection->connect_errno . ' - Mensagem: ' . mysqli_connect_error();
+    }
 
-  //   return $this->conexao;
-  // }
-
-  // function closeConexao()
-  // {
-  //   $status = false;
-  //   if (isset($this->conexao)) {
-  //     $status = mysqli_close($this->conexao);
-  //   }
-
-  //   return $status;
-  // }
+    return "Você está conectado";
+  }
 }
